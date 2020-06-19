cd71c047491aae64e253a2017b405c7a0100dab1
mashood100 <mashoodhussain100@gmail.com>
2020-06-20 04:38:47 +0500

    a

---
+++ app.js
@@ -0,0 +1,84 @@
+// // ======= chapter 25 to 25
+// var user_name = prompt("What's your full name ");
+// alert("GOOD MORNING, "+ user_name)
+
+
+// // task 2
+// var user_phone = prompt("What's your favraite mobile phone model"),
+//     len = user_phone.length;
+
+// document.write("My favraite mobile phone is " + user_phone + "<br> and string length is " + len);    
+
+// //  task 3
+
+// var country = "Pakistan";
+
+// document.write("String : "+ country);
+// document.write("<br> Index no of : "+ country.indexOf("n"));
+
+// // task 4
+
+// var country = "Hellow world";
+
+// document.write("String : "+ country);
+// document.write("<br> Last occurance of leter L: "+ country.lastIndexOf("l"));
+// var country = "Pakistan";
+// document.write("String : "+ country);
+// document.write("Character at 3rd position is: "+ country.charAt(3));
+
+// // task 6
+
+// var greet = ["Good Morning "],
+//     name = [user_name];
+//     alert("<br> " +greet.concat(name));
+
+// // task 7
+
+
+// var city = "hyderbad"
+// var replace  = city.replace("hyder","islam")
+
+// document.write("<br> City name " + city + " <br> After replace " + replace)
+
+// task 8
+
+// var city = "Ali and Sami are best friends. They play cricket and football together"
+
+// var replace =city.replace(/and|_/g,'&');
+
+// document.write("<br> <b>Sentance before replacement:</b>  "+city)
+// document.write("<br > <b>Sentance after replacement</b>   " + replace)
+
+// task 9
+
+// var value_str = "456";
+// var value_num = Number(value_str)
+
+// document.write("Value: " + value_str + "<br> Type: " + typeof value_str);
+// document.write("<br>  Value: " + value_num + "<br> Type: " + typeof value_num);
+
+
+// task 10
+
+// var user = "peanut",
+//     user_upper = user.toProperCase();
+
+// document.write("<br> User input: " + user +"<br> Uppercase: " + user_upper);
+    
+//task 11
+
+// var value_num = 33.36;
+
+
+// var value_str = String(value_num)
+// var value_str = value_str.replace(/.|_/g,'3')
+// document.write("Number: " + value_num );
+
+// document.write("<br>  String: " + value_str );
+
+var user_name =prompt("Enter User name ")
+
+if( user_name.indexOf("@") ===-1 || user_name.indexOf("@") ===-1 || user_name.indexOf("@") ===-1 || user_name.indexOf("@") ===-1 )
+
+
+
