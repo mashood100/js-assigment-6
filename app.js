987fdc2f9bdb5b516fe92ee5a2c205d70a2cc221
mashood100 <mashoodhussain100@gmail.com>
2020-06-24 03:46:02 +0500

    Update app.js

--- app.js
+++ app.js
@@ -445,3 +445,12 @@ var weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 //     return longestWord;
 //   }
 //  alert( findLongestWord("web development course"))
+
+
+// task 13
+function countOccurences(string, word) {
+   return string.split(word).length - 1;
+}
+var text="We went down to the stall, then down to the river."; 
+var count=countOccurences(text,"we"); // 2
+alert(count)
