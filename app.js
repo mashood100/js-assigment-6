fc37a838792922d805377b1cf4e8f2ad599751b2
mashood100 <mashoodhussain100@gmail.com>
2020-06-23 23:08:47 +0500

    Update app.js

--- app.js
+++ app.js
@@ -76,9 +76,163 @@
 
 // document.write("<br>  String: " + value_str );
 
-var user_name =prompt("Enter User name ")
+// var user_name =prompt("Enter User name ")
 
-if( user_name.indexOf("@") ===-1 || user_name.indexOf("@") ===-1 || user_name.indexOf("@") ===-1 || user_name.indexOf("@") ===-1 )
+// if( user_name.indexOf("@") ===-1 || user_name.indexOf(".") ===-1 || user_name.indexOf(",") ===-1 || user_name.indexOf("!") ===-1 ){
+//     alert("Your user name is valid")
 
 
+// }
+// else{
 
+//     alert("Please enter valid youser name")
+// }
+
+
+//// =========== chapter 26 to 30
+
+
+//task 1
+
+// var user_num = +prompt("Enter any positive number");
+// if(user_num > 0){
+//     document.write("Number: " +user_num)
+//     document.write("Round off: " +Math.round(user_num))
+//     document.write("Floor value: " + Math.floor(user_num))
+//     document.write("celi value: " + Math.ceil(user_num))
+//     }
+//     else{
+//         alert("enter negative number")
+//     }
+    
+// //task 2
+
+// var user_num = +prompt("Enter nagative number");
+// if(user_num <0){
+// document.write("Number: " +user_num)
+// document.write("Round off: " +Math.round(user_num))
+// document.write("Floor value: " + Math.floor(user_num))
+// document.write("celi value: " + Math.ceil(user_num))
+// }
+// else{
+//     alert("enter negative number")
+// }
+
+//task 3
+
+// var user_num = +prompt("Enter any number")
+// document.write("Absolute value: " + Math.abs(user_num))
+
+
+// task 4
+
+// var random = Math.random()*7
+// document.write("Dice value: " + Math.floor(random))
+
+// // task 5
+// var random = Math.random()*2
+// if ( Math.floor(random)  == 1){
+
+//     document.write("Dice value: head ")
+
+// }
+
+// else {
+
+//     document.write("Dice value: Tails ")
+
+// }
+
+// task 6
+
+// var random = Math.random()*101
+// document.write("Random value between 1 to 100 value: " + Math.floor(random))
+
+// task 7
+
+// var wegith = prompt("Enter you wegith in kgs")
+// document.write("Your weigth in kg is: " + parseFloat(wegith) + " Kilogram" )
+
+
+// task 8
+
+// var secret = Math.random()*11
+// var user = +prompt("Enter no between 1 to 10")
+// if (Math.floor(secret) == user){
+
+//     document.write("Congrats You guess the rigth number")
+// }
+// else{
+
+//     document.write("Sorry your guess is wrong")
+// }
+
+// ====== chapter 31 to 34
+
+//task 1 
+var date = new Date()
+// document.write(date);
+
+//task 2
+// var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+// alert("Current Month Is " +months[date.getMonth()]  )
+
+
+//task 3
+
+ var weekday = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
+// alert("Curent Day is " + weekday[date.getDay()])
+
+//task 4
+
+// if (weekday[date.getDay()] == "Sun" || weekday[date.getDay()] == "Sat"){
+//     alert("It's fun day")
+// }
+// else{
+//     alert("It's Not fun Day :(")
+// }
+
+// // task 5
+// if (date.getDay() <= 15   ){
+//     alert("It's First fifteen days of the month” if the date is less than 16th of the month un day")
+// }
+// else{
+//     alert("Last days of the month")
+// }
+
+// task 6
+
+
+// var milli = date.getTime()
+// var min = milli/60
+
+// document.write("<br>Current Date: " +date)
+// document.write("<br>Current Date: " + milli)
+// document.write("<br>Current Date: " + min)
+
+// task 7
+
+// var AM_PM = date.getHours()
+
+// if( AM_PM <= 11){
+//     alert("It's AM")
+// }
+// else{
+//     alert("Its's PM")
+// }
+
+// task 8
+
+// var last_date = new Date(2020, 12, 0)
+
+// alert(last_date)
+
+//task 9
+
+// var ramdan = new Date(2020, 3, 18,0)
+// var today = new Date().getTime(ramdan)
+// var days_later =   ramdan-today
+// alert( days_later + " Days has been passed since ramdan") 
+
+//task 10
